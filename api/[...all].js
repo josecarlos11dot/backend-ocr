84615317e6d62d304180820667a3b5dc0309d1b0
José Arzate <arzajose@gmail.com>
2025-08-12 22:51:51 -0600

    fix: proper runtime and export in [...all].js

--- api/[...all].js
+++ api/[...all].js
@@ -1,2 +1,7 @@
-// Redirige cualquier /api/* a tu servidor Express
-module.exports = require('./index.js');
+import handler from './index.js';
+
+export const config = {
+  runtime: 'nodejs20.x' // Fuerza el entorno Node 20
+};
+
+export default handler;
